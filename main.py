7330c816c2a884eea6abd5c1ed842334d3cfb739
cc3389 <932043913@qq.com>
2023-01-04 11:22:26 +0800

    现在可以爬取所有小说板块

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def block():
                            "2020-荆棘鸟的填坑计划": "https://www.jingjiniao.info/forum-108-1.html",
                            "重度区": "https://www.jingjiniao.info/forum-95-1.html",
                            "短篇老区": "https://www.jingjiniao.info/forum-69-1.html",
-                           "2019-荆棘鸟的校园计划": "https://www.jingjiniao.info/forum-83-1.html"}
+                           "2019-荆棘鸟的校园计划": "https://www.jingjiniao.info/forum-83-1.html",
+                           "入园投票区":"https://www.jingjiniao.info/forum-102-1.html"}
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         results = [executor.submit(forum.main_spider, headers, key, value) for (key, value) in block_link_dict.items()]
         concurrent.futures.wait(results)
