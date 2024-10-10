6c41a47a1b5ba4e67d7d621a7f737a9222abf074
cc3389 <932043913@qq.com>
2024-10-10 23:22:23 +0800

    改进论坛异常处理

--- forum.py
+++ forum.py
@@ -53,102 +53,106 @@ def main_spider(block_name, block_url, hash_map):
             update_timeList = []
             if page_num > int(last_page_num):
                 break
-            response = make_request(base_url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            not_login = soup.find_all(text="抱歉，您尚未登录，没有权限访问该版块")
-            if len(not_login) != 0:
-                print('抱歉，您尚未登录，没有权限访问该版块')
-                return
-            # 获取本页中的所有文章链接
-            links = soup.select('.s.xst')
-            commentTags = soup.select(".acgifnums")
+            try:
+                response = make_request(base_url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                if len(soup.select("#messagetext")) > 0:
+                    error_message = soup.select("#messagetext")[0].text + soup.select("#messagetext")[
+                        0].next_sibling.text
+                    raise Exception(error_message)
+                # 获取本页中的所有文章链接
+                links = soup.select('.s.xst')
+                commentTags = soup.select(".acgifnums")
 
-            # 获取评论数和浏览数
-            for comment in commentTags:
-                a_tag = comment.find('a', class_="xi2")
-                commentList.append(a_tag.text)
-                viewTag = comment.find("span")
-                viewList.append(viewTag.text)
+                # 获取评论数和浏览数
+                for comment in commentTags:
+                    a_tag = comment.find('a', class_="xi2")
+                    commentList.append(a_tag.text)
+                    viewTag = comment.find("span")
+                    viewList.append(viewTag.text)
 
-            # 获取作者列表
-            author_divs = soup.find_all('div', class_='acgifby1')
-            for tag in author_divs:
-                for link in tag.find_all('a'):
-                    authorList.append(link.text)
-            if len(links) == 0:
-                print("找不到链接，可能是cookie有误或过期了")
-            for link in links:
-                # 获取标题
-                titleList.append(re.sub(r'\[最后更新:.*]', '', link.text))
-                # 获取更新时间
-                span_tag = link.find('span')
-                if span_tag is None:
-                    # 更新时间在标题中
-                    match = re.search(r'\[(最后更新|Last update):\s*(\d{4}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2})\]', link.text)
-                    if match:
-                        time_str = match.group(2)
-                        update_time = time_str
+                # 获取作者列表
+                author_divs = soup.find_all('div', class_='acgifby1')
+                for tag in author_divs:
+                    for link in tag.find_all('a'):
+                        authorList.append(link.text)
+                if len(links) == 0:
+                    print("找不到链接，可能是cookie有误或过期了")
+                for link in links:
+                    # 获取标题
+                    titleList.append(re.sub(r'\[最后更新:.*]', '', link.text))
+                    # 获取更新时间
+                    span_tag = link.find('span')
+                    if span_tag is None:
+                        # 更新时间在标题中
+                        match = re.search(r'\[(最后更新|Last update):\s*(\d{4}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2})\]', link.text)
+                        if match:
+                            time_str = match.group(2)
+                            update_time = time_str
+                        else:
+                            update_time = "1990-1-1 00:00"
                     else:
-                        update_time = "1990-1-1 00:00"
-                else:
-                    update_time = span_tag.get('title')
-                update_timeList.append(update_time)
+                        update_time = span_tag.get('title')
+                    update_timeList.append(update_time)
 
-                # 获取tid
-                tid_list = re.findall('(?<=tid=)([^&]+)', link.attrs['href'])
-                if tid_list:
-                    tid = tid_list[0]
-                else:
-                    tid_list = re.findall("(?<=thread-)[0-9]+-", link.attrs['href'])
+                    # 获取tid
+                    tid_list = re.findall('(?<=tid=)([^&]+)', link.attrs['href'])
                     if tid_list:
-                        tid = (tid_list[0]).replace("-", "")
+                        tid = tid_list[0]
                     else:
-                        tid = None
-                tidList.append(tid)
-            uidLinkTags = soup.findAll('a', attrs={'cs': '1'}, href=re.compile('uid'))
-            if page_num == 1:
-                # 去除 admin
-                if len(tidList) < len(uidLinkTags):
-                    authorList.pop(0)
-                    uidLinkTags.pop(0)
-                last_page_tag = soup.find("span", title=re.compile("共 [0-9]+ 页"))
-                last_page_num = last_page_tag.text.replace(" ", "").replace("/", "").replace("页", "")
-            print("正在爬取" + block_name + "板块第" + str(page_num) + "页", "/共", last_page_num, "页")
-            # 获取uid
-            for uidLinkTag in uidLinkTags:
-                uid = re.findall('(?<=uid[-=])([^\.]+)', uidLinkTag.attrs['href'])[0]
-                uidList.append(uid)
-            urlList = []
-            # 获取文章链接列表
-            for uid, tid, update_time in zip(uidList, tidList, update_timeList):
-                if tid is None:
-                    continue
-                url = 'https://www.jingjiniao.info/forum.php?mod=viewthread&tid=' + tid + '&page=1&authorid=' + uid
-                urlList.append(url)
-                # 增量有问题，先注释掉
-                # if tid not in hash_map or compare_time_str(hash_map[tid], update_time):
-                #     linkList.append(url)
-                #     hash_map[tid] = update_time
-            # print("{}的第{}页需要更新{}篇".format(block_name, page_num, len(temp_urlList)))
-            for link in urlList:
-                future = executor.submit(thread_spider, link, block_name)
-                future_list.append(future)
-            total_tidList.extend(tidList)
-            total_viewList.extend(viewList)
-            total_uidList.extend(uidList)
-            total_titleList.extend(titleList)
-            total_authorList.extend(authorList)
-            total_update_timeList.extend(update_timeList)
-            total_commentList.extend(commentList)
-            total_linkList.extend(urlList)
-            # 找到下一页
-            nextLinkTag = soup.select('.nxt')
-            if nextLinkTag:
-                nextLinkStr = nextLinkTag[0].attrs['href']
-                base_url = nextLinkStr
-                page_num = page_num + 1
-            else:
-                break
+                        tid_list = re.findall("(?<=thread-)[0-9]+-", link.attrs['href'])
+                        if tid_list:
+                            tid = (tid_list[0]).replace("-", "")
+                        else:
+                            tid = None
+                    tidList.append(tid)
+                uidLinkTags = soup.findAll('a', attrs={'cs': '1'}, href=re.compile('uid'))
+                if page_num == 1:
+                    # 去除 admin
+                    if len(tidList) < len(uidLinkTags):
+                        authorList.pop(0)
+                        uidLinkTags.pop(0)
+                    last_page_tag = soup.find("span", title=re.compile("共 [0-9]+ 页"))
+                    last_page_num = last_page_tag.text.replace(" ", "").replace("/", "").replace("页", "")
+                print("正在爬取" + block_name + "板块第" + str(page_num) + "页", "/共", last_page_num, "页")
+                # 获取uid
+                for uidLinkTag in uidLinkTags:
+                    uid = re.findall('(?<=uid[-=])([^\.]+)', uidLinkTag.attrs['href'])[0]
+                    uidList.append(uid)
+                urlList = []
+                # 获取文章链接列表
+                for uid, tid, update_time in zip(uidList, tidList, update_timeList):
+                    if tid is None:
+                        continue
+                    url = 'https://www.jingjiniao.info/forum.php?mod=viewthread&tid=' + tid + '&page=1&authorid=' + uid
+                    urlList.append(url)
+                    # 增量有问题，先注释掉
+                    # if tid not in hash_map or compare_time_str(hash_map[tid], update_time):
+                    #     linkList.append(url)
+                    #     hash_map[tid] = update_time
+                # print("{}的第{}页需要更新{}篇".format(block_name, page_num, len(temp_urlList)))
+                for link in urlList:
+                    future = executor.submit(thread_spider, link, block_name)
+                    future_list.append(future)
+                total_tidList.extend(tidList)
+                total_viewList.extend(viewList)
+                total_uidList.extend(uidList)
+                total_titleList.extend(titleList)
+                total_authorList.extend(authorList)
+                total_update_timeList.extend(update_timeList)
+                total_commentList.extend(commentList)
+                total_linkList.extend(urlList)
+                # 找到下一页
+                nextLinkTag = soup.select('.nxt')
+                if nextLinkTag:
+                    nextLinkStr = nextLinkTag[0].attrs['href']
+                    base_url = nextLinkStr
+                    page_num = page_num + 1
+                else:
+                    break
+            except Exception as e:
+                print(f'爬取 {block_name}失败。原因： {e}')
+                return
     # 迭代future对象，按完成顺序获取结果
     for future in future_list:
         try:
@@ -162,7 +166,7 @@ def main_spider(block_name, block_url, hash_map):
         write_to_csv(total_titleList, total_authorList, total_commentList,
                      total_viewList, block_name, total_update_timeList, total_linkList, "data.csv", recommend_list, favorite_list)
     except Exception as e:
-        print('发生了未知错误，错误信息：', e)
+        print('保存csv数据失败，错误信息：', e)
 
 
 if __name__ == '__main__':
